5de2ac8338f850b08ba9771c41c65d2616193677
Krzysztof Burghardt <krzysztof@burghardt.pl>
2007-11-01 09:02:34 +0000

    address of FSF changed
    git-svn-id: file:///home/user/src/subversion/libfaketime/trunk@16 a2bba0ae-b596-43b1-9fb3-250380ad25cb

--- libfaketime.c
+++ libfaketime.c
@@ -2,7 +2,7 @@
    LibFakeTime library - fake time() library
    Copyright (c) 2002-2004,2007 Krzysztof Burghardt <krzysztof@burghardt.pl>
    
-   $Id: libfaketime.c,v 1.4 2007-11-01 08:52:42 kb Exp $
+   $Id: libfaketime.c,v 1.5 2007-11-01 09:02:34 kb Exp $
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
 #include "libfaketime.h"
